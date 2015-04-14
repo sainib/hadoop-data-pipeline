5d2529a4de0ee3df712553a3abaf3e8c6c77c316
sainib <bsaini@hortonworks.com>
2015-04-13 22:07:36 -0400

    fixed the runtest script with teh chmod.. need to change the workflow

--- scripts/runTest.sh
+++ scripts/runTest.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+su - hdfs -c "hdfs dfs -chmod 777 /user/ambari-qa/data_pipeline_demo/data"
+
 rm -rf /root/data_pipeline_demo/input/SV-sample*xml
 cp /app/hadoop-data-pipeline/input_data/SV-sample-1.xml /root/data_pipeline_demo/input/
 cp /app/hadoop-data-pipeline/input_data/SV-sample-2.xml /root/data_pipeline_demo/input/
