0ba6f5217c4a83c23e5e5d1fd1c4c792360b86a9
Sean Roberts <seanorama@gmail.com>
2015-09-04 15:40:21 +0100

    update hostname to localhost

--- scripts/submitEntities.sh
+++ scripts/submitEntities.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 project_root="/app/hadoop-data-pipeline"
+sed -i.bak "s/sandbox.hortonworks.com/$(hostname -f)/" ${project_root}/falcon/cluster/primaryCluster.xml
 su - ambari-qa -c "falcon entity -submit -type cluster -file ${project_root}/falcon/cluster/primaryCluster.xml"
 su - ambari-qa -c "falcon entity -submit -type feed -file ${project_root}/falcon/feeds/inputFeed.xml"
 su - ambari-qa -c "falcon entity -submit -type process -file ${project_root}/falcon/process/processData.xml"
