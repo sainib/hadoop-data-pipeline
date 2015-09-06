0bde9cfc553b4c460d17139251bd307da4b8dda2
Sean Roberts <seanorama@gmail.com>
2015-09-06 18:41:52 +0100

    no message

--- scripts/resetAtlas.sh
+++ scripts/resetAtlas.sh
@@ -4,4 +4,4 @@ python /usr/hdp/current/atlas-server/bin/atlas_stop.py
 rm -rf /var/lib/atlas/data
 python /usr/hdp/current/atlas-server/bin/atlas_start.py
 sleep 10
-python /usr/hdp/current/atlas-server/atlas/bin/quick_start.py
+python /usr/hdp/current/atlas-server/bin/quick_start.py
