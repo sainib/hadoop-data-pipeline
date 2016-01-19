44f0676c01a87cf99ab1b031f6d87f2f45742727
root <root@sandbox.hortonworks.com>
2016-01-19 18:28:24 +0000

    Changed Submit Entities to use the demo user in conf file

--- scripts/submitEntities.sh
+++ scripts/submitEntities.sh
@@ -5,6 +5,8 @@ source "${__dir}/../conf"
 project_root="/app/hadoop-data-pipeline"
 sed -i.bak "s/sandbox.hortonworks.com/$(hostname -f)/" ${project_root}/falcon/cluster/primaryCluster.xml
 su - ${demo_user} -c "falcon entity -submit -type cluster -file ${project_root}/falcon/cluster/primaryCluster.xml"
+sed -i.bak "s/admin/${demo_user}/" ${project_root}/falcon/feeds/inputFeed.xml
 su - ${demo_user} -c "falcon entity -submit -type feed -file ${project_root}/falcon/feeds/inputFeed.xml"
+sed -i.bak "s/admin/${demo_user}/" ${project_root}/falcon/process/processData.xml
 su - ${demo_user} -c "falcon entity -submit -type process -file ${project_root}/falcon/process/processData.xml"
 
